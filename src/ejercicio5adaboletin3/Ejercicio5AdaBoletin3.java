eea7673a2649c369bb530d00b03342e75f8b0fb6
Amarico <Amarico@equipo-zalman>
2018-08-31 22:50:13 +0200

    Necesitamos mostrar un contador con 5 dígitos (X-X-X-X-X), que muestre los números del 0-0-0-0-0 al 9-9-9-9-9, con la particularidad que cada vez que aparezca un 3 lo sustituya por una E.

---
+++ src/ejercicio5adaboletin3/Ejercicio5AdaBoletin3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio5adaboletin3;
+
+/**
+ *
+ * @author Amarico
+ */
+public class Ejercicio5AdaBoletin3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Necesitamos mostrar un contador con 5 dígitos (X-X-X-X-X), 
+        //que muestre los números del 0-0-0-0-0  al 9-9-9-9-9,
+        // con la particularidad que cada vez que aparezca un 3 lo sustituya por una E.
+        
+       
+    }
+    
+}
